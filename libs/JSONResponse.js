7aaf4f50a57ddc301a6d4391eb0318a0db946bd9
qodirtok <qodirtok@gmail.com>
2021-04-01 10:12:53 +0700

    Update JSONResponse.js

--- libs/JSONResponse.js
+++ libs/JSONResponse.js
@@ -1,14 +1,16 @@
 var success = function (req, res, message, data) {
   res.status(200).json({
     code: 200,
+    status: "success",
     message: message || "success",
     data: data,
   });
 };
 
 var serverError = function (req, res, message, data) {
-  res.status(500).json({
-    code: 500,
+  res.status(400).json({
+    code: 400,
+    status: "error",
     message: message || "internal server error",
     data: data,
   });
